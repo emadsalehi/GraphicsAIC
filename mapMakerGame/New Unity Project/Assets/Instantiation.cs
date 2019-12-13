d589013d624f7b5438655f1232609a91d1d7ca36
SinaBolouki <sina.bolouki@gmail.com>
2019-12-13 23:18:04 +0330

    Game mapMaker Project added

---
+++ mapMakerGame/New Unity Project/Assets/Instantiation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiation : MonoBehaviour
+{
+    public GameObject emptyCell;
+    public int MapSize;
+    GameObject[,] cells;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cells = new GameObject[MapSize, MapSize];
+        for (int i = 0; i < MapSize; i++)
+        {
+            for (int j = 0; j < MapSize; j++)
+            {
+                Quaternion rot = Quaternion.Euler(90, 0, 0);
+                cells[i, j] = (GameObject) Instantiate(emptyCell, new Vector3(i - (MapSize / 2), 0, j - (MapSize / 2)), rot);
+          
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
