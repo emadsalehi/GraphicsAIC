7820f26e80365dbea0b6c303a03d5e2eb81ce957
SinaBolouki <sina.bolouki@gmail.com>
2019-12-15 00:04:32 +0330

    mapMaker Camera problem solved, size selection menu added

--- mapMakerGame/New Unity Project/Assets/Instantiation.cs
+++ mapMakerGame/New Unity Project/Assets/Instantiation.cs
@@ -1,25 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Instantiation : MonoBehaviour
 {
     public GameObject emptyCell;
-    public int MapSize;
+    public Text HeightText;
+    public Text WidthText;
+    public GameObject menuGameObject;
     GameObject[,] cells;
     // Start is called before the first frame update
-    void Start()
+    public void makeMap()
     {
-        cells = new GameObject[MapSize, MapSize];
-        for (int i = 0; i < MapSize; i++)
+        int HeightSize = int.Parse(HeightText.text.ToString());
+        int WidthSize = int.Parse(WidthText.text.ToString());
+        cells = new GameObject[HeightSize, WidthSize];
+        CanvasGroup getCanvasGroup = menuGameObject.GetComponent<CanvasGroup>();
+        for (int i = 0; i < HeightSize; i++)
         {
-            for (int j = 0; j < MapSize; j++)
+            for (int j = 0; j < WidthSize; j++)
             {
-                Quaternion rot = Quaternion.Euler(90, 0, 0);
-                cells[i, j] = (GameObject) Instantiate(emptyCell, new Vector3(i - (MapSize / 2), 0, j - (MapSize / 2)), rot);
+                Quaternion rot = Quaternion.Euler(0, 0, 0);
+                cells[i, j] = (GameObject) Instantiate(emptyCell, new Vector3(i + 0.5f , j + 0.5f, 0), rot);
           
             }
         }
+
+        menuGameObject.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
---
+++ mapMakerGame/New Unity Project/Assets/MouseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Debug.Log(mousePos);
+            //mousePos.y = 0;
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mousePos);
+            Debug.Log("mouse" + mousePosition);
+            Debug.Log(Input.mousePosition);
+        }
+        int screenWidth = Screen.width;
+        int screenHeight = Screen.height;
+
+    }
+    
+}
