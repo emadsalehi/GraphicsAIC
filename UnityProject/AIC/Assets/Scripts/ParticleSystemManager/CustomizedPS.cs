9c2a218f2bbc3bb467ec7c316befd86734af9c76
Mohammad Sadra Heydari <45513846+sadraheydari@users.noreply.github.com>
2019-12-18 21:45:55 +0330

    ParticleSystemManager
    This version is not final.

---
+++ UnityProject/AIC/Assets/Scripts/ParticleSystemManager/CustomizedPS.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CustomizedPS 
+{
+    public string name;
+
+    [Range(0f, 10f)]
+    public float time;
+
+    public GameObject gameObject;
+
+    public CustomizedPS(CustomizedPS cps) {
+        this.name = cps.name;
+        this.time = cps.time;
+    }
+
+    public void Update() {
+        this.time -= Time.deltaTime;
+        if(time <= 0) {
+            this.GetParticleSystem().Stop();
+        }
+    }
+
+    public bool isFinished() {
+        return time <= 0 && !this.GetParticleSystem().IsAlive();
+    }
+
+    public ParticleSystem GetParticleSystem() {
+        return gameObject.GetComponent<ParticleSystem>();
+    }
+
+}
---
+++ UnityProject/AIC/Assets/Scripts/ParticleSystemManager/ParticleSystemManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ParticleSystemManager : MonoBehaviour
+{
+
+    public static ParticleSystemManager instance;
+    public CustomizedPS[] systems;
+    private static List<CustomizedPS> playingSystems = new List<CustomizedPS>();
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            Play("sadra", new Vector3(10, 10, 10));
+        }
+        playingSystems.ForEach(element => element.Update());
+        deleteFinishedSystems();
+
+    }
+
+    public void Play(string name, Vector3 position)
+    {
+        CustomizedPS temp = Array.Find(systems, item => item.name == name);
+        if (temp == null)
+        {
+            Debug.LogError("Particle System: " + name + "not found!");
+            return;
+        }
+        playingSystems.Add(cloneCPS(temp, position, temp.GetParticleSystem().transform.rotation));
+    }
+
+    public void Play(string name,  Vector3 position, Quaternion rotation)
+    {
+        CustomizedPS temp = Array.Find(systems, item => item.name == name);
+        if (temp == null)
+        { 
+            Debug.LogError("Particle System: " + name + "not found!");
+            return;
+        }
+
+        // if(rotation == null) {
+        //     rotation = (Quaternion)temp.GetParticleSystem().transform.rotation;
+        //     playingSystems.Add(cloneCPS(temp, position, temp.GetParticleSystem().transform.rotation));
+        // }
+
+        playingSystems.Add(cloneCPS(temp, position, rotation));
+    }
+
+
+    private static CustomizedPS cloneCPS(CustomizedPS cps, Vector3 position, Quaternion rotation)
+    {
+        CustomizedPS result = new CustomizedPS(cps);
+        result.gameObject = (GameObject)Instantiate(cps.gameObject, position, rotation);
+        return result;
+    }
+
+    private static void deleteFinishedSystems()
+    {
+        for (int i = 0; i < playingSystems.Count; i++)
+        {
+            if (playingSystems[i].isFinished())
+            {
+                CustomizedPS temp = playingSystems[i];
+                playingSystems.RemoveAt(i);
+                i--;
+                Destroy(temp.gameObject);
+            }
+        }
+    }
+}
