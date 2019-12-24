22f0a87da6ee2592f93d1af8b4e4f44d2d5d37aa
Emad Salehi <emad.s1178@yahoo.com>
2019-12-24 21:14:52 +0330

    Move and Attack effect controllers added

---
+++ UnityProject/AIC/Assets/Scripts/ParticleSystemManager/AttackEffectController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+public class dfsdf : MonoBehaviour
+{
+    public GameObject particleSystemGameObject;
+    public float offsetLocationX;
+    public float offsetLocationY;
+    public float offsetLocationZ;
+    public float distance;
+    public Vector3 destination;
+    public GameObject target;
+
+    private ParticleSystem particeSystem;
+
+
+
+    public void SetTarget(GameObject target)
+    {
+        if (target!=null){
+            this.target = target;
+        }
+    }
+
+    void Start()
+    {   
+        particleSystemGameObject.transform.position += new Vector3(offsetLocationX, offsetLocationY, offsetLocationZ);
+        particeSystem = particleSystemGameObject.GetComponent<ParticleSystem>();
+        if (target!=null){
+
+        destination = target.transform.position;
+        }
+        
+	}
+  
+
+    public void StopParticleSystem() {
+        particeSystem.Stop();
+    }
+
+    public void PlayParticleSystem(Vector3 destination) {
+        if (target!=null){
+            destination -= particleSystemGameObject.transform.position;
+            // distance = destination.magnitude;
+            // var main = particeSystem.main;
+            // float speed = main.startSpeed.Evaluate(0.0f);
+            // main.startLifetime = distance / speed;
+            particeSystem.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target!=null){
+            Vector3 tp = target.transform.position;
+            particleSystemGameObject.transform.LookAt(tp);
+        }
+    }
+}
---
+++ UnityProject/AIC/Assets/Scripts/UnitController/MoveController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isMoving ; 
+    public bool isRotating;
+    public float turnTime ; 
+    public bool isMovingAfterRotate;
+    
+    public Vector3 direction ; 
+    public float valueOfRotate;
+    public float speed = 1.0f;
+    
+
+    void StartMoving(Vector3 direction){
+        this.direction = direction / direction.magnitude;
+        isMoving = true;
+        isRotating = false ; 
+        isMovingAfterRotate = false;
+
+    }
+    
+    void StartMovingAfterRotate(Vector3 direction)
+    {
+        this.direction = direction / direction.magnitude;
+        isMovingAfterRotate = true;
+        isRotating = false;
+        isMoving = false;     
+    }
+    
+    void StopEveryThing()
+    {
+        isMoving = false;
+        isRotating = false;
+        isMovingAfterRotate = false;
+    }
+    
+    void StartRotating(float degree)
+    {
+        isMoving = false;
+        isRotating = true;
+        isMovingAfterRotate = false;
+        valueOfRotate = degree / (turnTime / 4);
+    }
+
+    void Start()
+    {
+
+        // Calculate the journey length.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving){
+            transform.position += direction * speed * Time.deltaTime / turnTime;
+        }
+        if (isRotating){
+            transform.Rotate(0, valueOfRotate * Time.deltaTime, 0);
+        }  
+        if (isMovingAfterRotate)
+        {
+            transform.position += direction * speed * Time.deltaTime / (turnTime * 3 / 4);
+        }      
+    }
+}
