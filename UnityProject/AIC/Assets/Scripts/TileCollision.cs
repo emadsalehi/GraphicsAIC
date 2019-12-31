b1f9205de5c8d770cba754d53c71d0570f59043c
Mohammad Sadra Heydari <45513846+sadraheydari@users.noreply.github.com>
2019-12-31 18:24:39 +0330

    Create TileCollision.cs

---
+++ UnityProject/AIC/Assets/Scripts/TileCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileCollision : MonoBehaviour
+{
+
+    private Rigidbody rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (GetComponent<Collider>().transform.position.y > 5 * GetComponent<Collider>().bounds.size.y)
+            return;
+        rigidBody.velocity = new Vector3(0, 0, 0);
+        Destroy(GetComponent<Rigidbody>());
+        Destroy(GetComponent<Collider>());
+    }
+}
