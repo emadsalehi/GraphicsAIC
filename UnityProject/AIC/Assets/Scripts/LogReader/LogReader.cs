075ef845e48acdc4bc8128acf8a454f76f6d29ce
emad salehi <emad.s1178@yahoo.com>
2020-01-08 01:25:13 +0330

    Towers and SpellEffectController added

--- UnityProject/AIC/Assets/Scripts/LogReader/LogReader.cs
+++ UnityProject/AIC/Assets/Scripts/LogReader/LogReader.cs
@@ -104,7 +104,7 @@ public class LogReader : MonoBehaviour
     public Game ReadLog()
     {
         Game gameLog;
-        using (StreamReader r = new StreamReader("Assets/Scripts/Log/graphic2.json"))
+        using (StreamReader r = new StreamReader("Assets/Scripts/Log/graphic3.json"))
         {
             string json = r.ReadToEnd();
             gameLog = JsonConvert.DeserializeObject<Game>(json);
