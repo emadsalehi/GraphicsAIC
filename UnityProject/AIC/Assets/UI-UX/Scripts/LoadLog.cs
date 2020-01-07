e42abf8bdc264c9f279e2be2b7187e8c00cf969b
SinaBolouki <sina.bolouki@gmail.com>
2020-01-07 23:08:53 +0330

    Map Env prefabs and menu scenes added

---
+++ UnityProject/AIC/Assets/UI-UX/Scripts/LoadLog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LoadLog : MonoBehaviour
+{
+    public GameObject mainMenu, selectLog, playMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
