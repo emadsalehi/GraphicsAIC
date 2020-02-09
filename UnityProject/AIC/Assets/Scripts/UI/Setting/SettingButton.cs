fe121e08b8b8fa585d2b097dfa5e98885de14912
ashkanlarni <ash.larni@gmail.com>
2020-02-09 21:54:12 +0330

    Endgame panel is done.

---
+++ UnityProject/AIC/Assets/Scripts/UI/Setting/SettingButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SettingButton : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        
+        var btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(OnMouseClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseClick()
+    {
+        var ui = GameObject.Find("In-Game UI");
+        ui.SetActive(false);
+        var setting = GameObject.Find("Settings Menu");
+        GameObject.Find
+        setting.SetActive(false);
+    }
+}
