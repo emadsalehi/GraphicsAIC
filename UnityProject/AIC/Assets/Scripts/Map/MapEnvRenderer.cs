81285e72978be131a0c7a53a27b40d3b87112f72
SinaBolouki <sina.bolouki@gmail.com>
2020-02-13 00:52:24 +0330

    Map Env Renderer Added

---
+++ UnityProject/AIC/Assets/Scripts/Map/MapEnvRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEnvRenderer : MonoBehaviour
+{
+    public GameObject[] MapEnvs;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RenderMapEnv(int height, int width)
+    {
+        String envName = height + "by" + width + " Normalized";
+        foreach (GameObject mapEnv in MapEnvs)
+        {
+            if (mapEnv.name == envName)
+            {
+                Instantiate(mapEnv);
+                break;
+            }
+        }
+    }
+}
